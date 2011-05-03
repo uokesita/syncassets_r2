58db76a61b9d71e7c911c796c31cff232a303cf9
Osledy Bazo <osledybazo@gmail.com>
2011-05-03 17:14:18 -0430

    removieng railtie

--- lib/railtie.rb
+++
@@ -1,9 +0,0 @@
-require 'syncassets_r2'
-require 'rails'
-
-class Railtie < Rails::Railtie
-  rake_tasks do
-    load "tasks/syncassets_r2.rake"
-  end
-end
-
--- rails/init.rb
+++
@@ -1,1 +0,0 @@
-require 'syncassets_r2/railtie'
